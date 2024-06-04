4d29dfab3e14680ba95d490ea903396c83f4c07d
PamTPK <6431501044@lamduan.mfu.ac.th>
2024-06-04 15:40:08 +0700

    อัพไฟลืครั้งที่ 1

---
+++ jason/loadlayoutprofile.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/layouts/layoutprofile.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+});
+
